e5ac10d3b7abbdcf5a79d7984edb9736d2797727
katrox <wildcityserver@qq.com>
2023-12-20 18:58:29 +0800

    修正目录格式

--- client/js/index/log.js
+++ client/js/index/log.js
@@ -1,43 +1,43 @@
 const autolog = $('autolog');
 
 document.addEventListener('keyup', function(event) {
-	if (event.keyCode == 13)
-		log();
+    if (event.keyCode == 13)
+        log();
 });
 
 if ((user = localStorage.getItem('username')) != null) {
     // 记住账号功能
-	username.value = user;
-	// 保持登录功能
-	if (localStorage.getItem('autolog') == 'true' && (pswd = localStorage.getItem('password')) != null) {
-		password.value = pswd;
-		autolog.checked = true;
-		log();
-	} else password.focus();
+    username.value = user;
+    // 保持登录功能
+    if (localStorage.getItem('autolog') == 'true' && (pswd = localStorage.getItem('password')) != null) {
+        password.value = pswd;
+        autolog.checked = true;
+        log();
+    } else password.focus();
 }
 
 function log() {
-	const user = username.value;
-	const pswd = password.value;
-	
-	var flag = false;
-	if (user == '') {
-		remindUsername('账号不能为空');
-		shake(document.getElementsByClassName('ipt-box')[0]);
-		flag = true;
-	}
-	if (pswd == '') {
-		remindPassword('密码不能为空');
-		shake(document.getElementsByClassName('ipt-box')[1]);
-		flag = true;
-	}
-	if (flag)
-		return;
-	
-	localStorage.setItem('username', user);
-	
-	// 提交到数据库
-	verifyPswd(user, pswd);
+    const user = username.value;
+    const pswd = password.value;
+    
+    var flag = false;
+    if (user == '') {
+        remindUsername('账号不能为空');
+        shake(document.getElementsByClassName('ipt-box')[0]);
+        flag = true;
+    }
+    if (pswd == '') {
+        remindPassword('密码不能为空');
+        shake(document.getElementsByClassName('ipt-box')[1]);
+        flag = true;
+    }
+    if (flag)
+        return;
+    
+    localStorage.setItem('username', user);
+    
+    // 提交到数据库
+    verifyPswd(user, pswd);
 }
 
 // 登录成功
@@ -54,9 +54,9 @@ function logSuccess() {
     }, 1500);
 
     // 记录是否保持登录
-	localStorage.setItem('autolog', autolog.checked);
-	if (autolog.checked)
-		localStorage.setItem('password', password.value);
+    localStorage.setItem('autolog', autolog.checked);
+    if (autolog.checked)
+        localStorage.setItem('password', password.value);
 }
 
 let failureCount = 0;       // 登录失败次数
@@ -69,22 +69,22 @@ function logFailure() {
         closeable: true
     });
     password.value = '';
-	password.focus();
+    password.focus();
 
     // 失败超过5次进入冷却
-	if (++failureCount >= 5) {
-	    failureCount = 0;
-	    let btn = $('login');
-	    let sec = 30;
-	    const txt = btn.innerText;
-	    btn.disabled = true;
-	    btn.innerText = sec + '秒后重试';
-	    let timer = setInterval(function() {
-	        if (--sec <= 0) {
+    if (++failureCount >= 5) {
+        failureCount = 0;
+        let btn = $('login');
+        let sec = 30;
+        const txt = btn.innerText;
+        btn.disabled = true;
+        btn.innerText = sec + '秒后重试';
+        let timer = setInterval(function() {
+            if (--sec <= 0) {
                 clearInterval(timer);
                 btn.innerText = txt;
                 btn.disabled = false;
             } else btn.innerText = sec + '秒后重试';
-	    }, 1000);
-	}
+        }, 1000);
+    }
 }
