d6628eaac1ef7d616f054349e037edc807be9d3c
Somebody <3514452840@qq.com>
2023-12-21 11:09:55 +0800

    董延昭 模糊搜索0

---
+++ client/js/dyz_main/search.js
@@ -0,0 +1,17 @@
+ function handleKeyPress(event) {
+      // 检测是否按下回车键
+      if (event.key === 'Enter') {
+        // 阻止默认的回车键行为（例如提交表单）
+        event.preventDefault();
+        // 执行搜索事件
+        performSearch();
+      }
+    }
+	
+function performSearch(){
+	var key_word = document.querySelector(".search-input").value.toLowerCase();
+	if(!key_word)
+		return ;
+	const searchResults = data.filter(item => item.toLowerCase().includes(key_word));//模糊搜索
+	
+}
